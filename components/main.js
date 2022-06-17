c77563e2a2a331a2f41ee0f39b53f941d8731ef4
srbhkumar624 <srbhkumar624@gmail.com>
2022-06-17 12:21:37 +0530

    added index file

---
+++ components/main.js
@@ -0,0 +1,113 @@
+function navbar(){
+    return `<div id="news">NEWS</div>
+    <div>LIVE TV</div>
+    <div id="top_img"><img src="https://akm-img-a-in.tosshub.com/sites/all/themes/itg/logo.png?v=1.52" alt=""></div>
+    <div>APP</div>
+    <div>MAGAZINE</div>
+</div>`
+}
+function navbar2(){
+    return`<div id="home">HOME</div>
+    <div>MY FEED</div>
+    <div id="india">INDIA</div>
+    <div id="world">WORLD</div>
+    <div id="business">BUSINESS</div>
+    <div id="tech">TECH</div>
+    <div id="health">HEALTH</div>
+    <div id="sports">SPORTS</div>
+    <div id="science">SCIENCE</div>
+    <div>BOARD RESULTS 2022</div>
+    <div><i class="fa-solid fa-grip-lines"></i></div>
+    <div id="business_icon"><i class="fa-solid fa-chart-line"></i></div>
+    <div id="entertain"><i class="fa-solid fa-tv"></i></div>
+    <div id="share"><i class="fa-solid fa-share-nodes"></i></div>
+    <div>
+    <i id="searchIcon" class="material-icons accent">search</i>
+    <input type="text" id="search">
+    <i id="clear" class="material-icons">clear</i>
+    <div><i class="fa-solid fa-xmark"></i></div>
+    </div>
+    <div id="user"><i class="fa-solid fa-user"></i></div>
+</div>`
+}
+function footer(){
+    return ` <div id="top">
+    <div><h3>INDIATODAY.IN</h3></div>
+    <div><i class="fa-brands fa-facebook-f"></i></div>
+    <div><i class="fa-brands fa-twitter"></i></div>
+    <div><i class="fa-solid fa-rss"></i></div>
+    
+</div>
+<div id="top1">
+    <ul>
+        <li><h4>PUBLICATIONS:</h4></li>
+        <li>Business Today</li>
+        <li>Cosmopolitan</li>
+        <li>India Today-Hindi</li>
+        <li>India Today</li>
+        <li>Mail Today</li>
+        <li>Money Today</li>
+        <li>Reader's Digest</li>
+        <li>Time</li>
+
+    </ul>
+    <ul>
+        <li><h4>TELEVISION:</h4></li>
+        <li>Aaj Tak</li>
+        <li>Delhi Aaj Tak</li>
+        <li>Good News Today</li>
+        <li>India Today Tv</li>
+        <li><h4>RADIO</h4></li>
+        <li>Ishq FM</li>
+        <li><h4>GAMING</h4></li>
+        <li>India Today Gaming</li>
+        <li>World Esports Cup</li>
+
+    </ul>
+    <ul>
+        <li><h4>EDUCATIONS:</h4></li>
+        <li>India Today Education</li>
+        <li>Vasant Valley</li>
+        <li>Best Colleges India 2018</li>
+        <li>Best University India 2018</li>
+        <li>Aaj Tak Campus National</li>
+        <li>Aptitude Test</li>
+        <li><h4>Online Shopping:</h4></li>
+
+    </ul>
+    <ul>
+        <li><h4>EVENTS:</h4></li>
+        <li>Agenda Aajtak</li>
+        <li>India Today Conclave</li>
+        <li>Robb Report India 2018</li>
+        <li>Sahitya Aaj Tak</li>
+        <li>The Red Lab</li>
+
+    </ul>
+    <ul>
+        <li><h4>PRINTING:</h4></li>
+        <li>Thomson Press</li>
+        <li><h4>WELFARE</h4></li>
+        <li>Care today</li>
+        <li><h4>MUSIC</h4></li>
+        <li>Music Today</li>
+
+    </ul>
+    <ul>
+        <li><h4>SYNDICATIONS:</h4></li>
+        <li>India Conters</li>
+        <li>Headliners today</li>
+        <li><h4>DISTRIBUTIONS:</h4></li>
+        <li>Rate Card</li>
+        <li><h4>USEFUL LINKS</h4></li>
+        <li>Money Today</li>
+        <li>Reader's Digest</li>
+        <li>Time</li>
+
+    </ul>
+    
+</div>`
+}
+
+    
+export{navbar,navbar2,footer};
---
+++ scripts/science.js
@@ -0,0 +1,110 @@
+import { navbar,navbar2,footer} from "../components/main.js";
+let nav1=document.getElementById("navbar");
+let nav2=document.getElementById("navbar2");
+let foot=document.getElementById("footer")
+nav1.innerHTML=navbar();
+nav2.innerHTML=navbar2();
+foot.innerHTML=footer();
+const url="https://newsdata.io/api/1/news?apikey=pub_8197fcabafb32999a0cccf108a2141410681&country=in&language=en&category=science";
+fetch(url)
+.then(function(res){
+    return res.json();
+})
+.then(function(res){
+    let data=res;
+    append(data.results);
+    console.log(data);
+})
+.catch(function(err){
+    console.log(err);
+})
+let div=document.getElementById("science_news");
+function append(data){
+    data.forEach(function(el){
+        let box=document.createElement("div");
+        let image=document.createElement("img")
+        image.src=el.image_url;
+        let title=document.createElement("h4");
+        title.innerText=el.title;
+        let date=document.createElement("h4");
+        date.innerText=el.pubDate;
+        let des=document.createElement("h4");
+        des.innerText=el.description;
+        let lang=document.createElement("h4");
+        lang.innerText=el.language;
+        box.append(image,title,date,des,lang);
+        div.append(box);
+    })
+}
+document.getElementById("user").addEventListener("click",function(){
+    loginUser();
+})
+function loginUser(){
+    window.location.href="login.html"
+}
+document.getElementById("entertain").addEventListener("click",function(){
+    entertainNews();
+})
+function entertainNews(){
+    window.location.href="entertain.html"
+}
+document.getElementById("business").addEventListener("click",function(){
+    businessNews();
+})
+function businessNews(){
+    window.location.href="business.html"
+}
+document.getElementById("business_icon").addEventListener("click",function(){
+    businessNews_icon();
+})
+function businessNews_icon(){
+    window.location.href="business.html"
+}
+document.getElementById("home").addEventListener("click",function(){
+    homeNews();
+})
+function homeNews(){
+    window.location.href="index.html";
+}
+document.getElementById("tech").addEventListener("click",function(){
+    techNews();
+})
+function techNews(){
+    window.location.href="tech.html";
+}
+document.getElementById("health").addEventListener("click",function(){
+    healthNews();
+})
+function healthNews(){
+    window.location.href="health.html";
+}
+document.getElementById("sports").addEventListener("click",function(){
+    sportsNews();
+})
+function sportsNews(){
+    window.location.href="sports.html";
+}
+document.getElementById("science").addEventListener("click",function(){
+    scienceNews();
+})
+function scienceNews(){
+    window.location.href="science.html";
+}
+document.getElementById("world").addEventListener("click",function(){
+    worldNews();
+})
+function worldNews(){
+    window.location.href="world.html"
+}
+document.getElementById("news").addEventListener("click",function(){
+    newNews();
+})
+function newNews(){
+    window.location.href="index.html"
+}
+document.getElementById("india").addEventListener("click",function(){
+    indiaNews()
+})
+function indiaNews(){
+    window.location.href="india.html"
+}
